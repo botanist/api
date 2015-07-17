60adf71d0f11b32fbe76ac03fa7777a927b7e45d
Claes Jakobsson <claes@surfar.nu>
2015-07-17 22:46:04 +0200

    Added more utility methods for creating messages

--- service/conn.go
+++ service/conn.go
@@ -37,12 +37,37 @@ func (s *Conn) Write(data interface{}) error {
 	return err
 }
 
+func (s *Conn) Hello(ver uint32) error {
+	return s.Write(HelloMsg{ProtocolVersion:ver})
+}
+
 func (s *Conn) Authenticate(typeId uint32, uuid string, key string) error {
 	return s.Write(AuthenticateMsg{TypeId: typeId, UUID: uuid, Key: key})
 }
 
+func (s *Conn) AuthenticationFailed(reason string) error {
+	return s.Write(AuthenticationFailedMsg{Reason:reason})
+}
+
+func (s *Conn) AuthenticationSucceeded(deviceId uint32, rfAddr uint16, newKey string) error {
+	return s.Write(AuthenticationSucceededMsg{DeviceId: deviceId, RFAddr: rfAddr, NewKey: newKey})
+}
+
+func (s *Conn) GetRFAddr(id uint32) error {
+	return s.Write(GetRFAddrMsg{DeviceId: id})
+}
+
+func (s *Conn) SendRFAddr(id uint32, addr uint16) error {
+	return s.Write(RFAddrMsg{DeviceId: id, RFAddr: addr})
+}
+
+/* Types */
 func (s *Conn) GetType(id uint32) error {
 	return s.Write(GetTypeMsg{TypeId: id})
+}
+
+func (s *Conn) SendNoType(id uint32) error {
+	return s.Write(NoTypeMsg{TypeId: id})
 }
 
 func (s *Conn) SendType(id uint32, src string, masks []uint32, intervals []uint8) error {
@@ -54,11 +79,39 @@ func (s *Conn) SendType(id uint32, src string, masks []uint32, intervals []uint8
 	})
 }
 
+/* JOINING */
 func (s *Conn) RequestJoin(UUID string, typeId uint32) error {
 	return s.Write(JoinRequestMsg{
 		TypeId: typeId,
 		UUID:   UUID,
 	})
+}
+
+func (s *Conn) JoinRequestPending(UUID string) error {
+	return s.Write(JoinRequestPendingMsg{UUID: UUID})
+}
+
+func (s *Conn) JoinRequestDeclined(UUID string) error {
+	return s.Write(JoinRequestDeclinedMsg{UUID: UUID})
+}
+
+func (s *Conn) JoinRequestApproved(UUID string, id uint32, addr uint16) error {
+	return s.Write(JoinRequestApprovedMsg{UUID: UUID, DeviceId: id, RFAddr: addr})
+}
+
+/* WIRED SENSORS */
+func (s *Conn) ConnectDeviceDeclined(UUID string, parentId uint32) error {
+	return s.Write(ConnectDeviceDeclinedMsg{UUID: UUID, ParentDeviceId: parentId})
+}
+
+func (s *Conn) ConnectDeviceApproved(UUID string, parentId, id uint32) error {
+	return s.Write(ConnectDeviceApprovedMsg{UUID: UUID, ParentDeviceId: parentId, DeviceId: id})
+}
+
+/* Utilities */
+
+func (s *Conn) RemoteAddr() net.Addr {
+	return s.conn.RemoteAddr()
 }
 
 func NewConn(conn net.Conn) (*Conn, error) {
